9da9f15666403bfcce98701c173b673475d1263a
jo-and75 <jaotika@gmail.com>
2024-05-20 12:09:02 +0300

    Add divs for rest of container

--- main.js
+++ main.js
@@ -4,9 +4,22 @@ const container = document.getElementById("container");
 
 for(let i =0; i < 16 ; i++){  
     const leftDivs = document.createElement("div");
-    leftDivs.setAttribute("style", "border:solid; height:25px; width:25px; margin: 0; padding:0")  
+    leftDivs.setAttribute("style", "border:solid; height:25px; width:25px; margin: 0; padding:0");  
     //Initially didn't work because divs were being created and not appended to the DOM after each loop so appendment had to be added in the loop. 
-    container.appendChild(leftDivs);
+    container.appendChild(leftDivs); 
+
+    const rightDivs = document.createElement("div");
+    rightDivs.setAttribute("style", "border:solid; height:25px; width:25px; margin: 0; padding:0");
+    container.appendChild(rightDivs);  
+
+    const topDivs = document.createElement("div");
+    topDivs.setAttribute("style", "border:solid; height:25px; width:25px; margin: 0; padding:0");
+    container.appendChild(topDivs);  
+
+    const bottomDivs = document.createElement("div");
+    bottomDivs.setAttribute("style", "border:solid; height:25px; width:25px; margin: 0; padding:0");
+    container.appendChild(bottomDivs);  
+
 }
 
 body.setAttribute("style", "background-color:#771011;")
