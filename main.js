f2098591aecb3f05dd02f785244f7dc5dce2767f
jo-and75 <jaotika@gmail.com>
2024-05-20 22:43:58 +0300

    Change box sizing to border-box
    - Removes excess space taken up by padding and borders

--- main.js
+++ main.js
@@ -7,22 +7,22 @@ container.setAttribute("style", "border: solid purple; display:flex;")
 
 function createGrid(size) {  
 
-    const containerWidth = size*25;
-    const containerHeight = size*25;
+    const containerWidth = size * 25;
+    const containerHeight = size * 25;
 
     container.style.width = containerWidth + "px"; // Need to add px cause css dictates unit needs to be defined
-    container.style.height = containerLength + "px";   
+    container.style.height = containerHeight + "px";   
 
 
 
-    for(let i = 1; i < size ; i++){    
+    for(let i = 0; i < size ; i++){    
     const column = document.createElement("div"); 
-    column.setAttribute("style","border: solid black; height: 25px; width: 25px;"); 
+    column.setAttribute("style","display: flex; flex-direction: column; box-sizing: border-box; border: solid black;"); 
     container.appendChild(column);   
 
-        for (let j = 1; j <= size; j++){ 
+        for (let j = 0; j < size; j++){ 
             const row = document.createElement("div"); 
-            row.setAttribute("style","border: solid black; height: 25px; width: 25px;"); 
+            row.setAttribute("style","border: solid black; height: 25px; width: 25px; margin: 0; padding: 0; box-sizing: border-box;"); 
             column.appendChild(row);
         }
     }  
