70349ecb786805eb786877a935c66feda9524c84
jo-and75 <jaotika@gmail.com>
2024-05-20 11:57:37 +0300

    Create container div & for loop to create multiple div boxes.

---
+++ main.js
@@ -0,0 +1,14 @@
+const body = document.querySelector("body");   
+const container = document.getElementById("container");
+
+
+for(let i =0; i < 16 ; i++){  
+    const div1 = document.createElement("div");
+    div1.setAttribute("style", "border:solid; height:25px; width:25px; margin: 0; padding:0") 
+}
+
+body.setAttribute("style", "background-color:#771011;")
+
+
+
+container.appendChild(div1);
