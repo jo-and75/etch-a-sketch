2e7dd3bf3335dcbd6fd35e13657c715b01b5d8c5
jo-and75 <jaotika@gmail.com>
2024-05-20 12:02:03 +0300

    Append divs to container in the for loop

--- main.js
+++ main.js
@@ -4,11 +4,12 @@ const container = document.getElementById("container");
 
 for(let i =0; i < 16 ; i++){  
     const div1 = document.createElement("div");
-    div1.setAttribute("style", "border:solid; height:25px; width:25px; margin: 0; padding:0") 
+    div1.setAttribute("style", "border:solid; height:25px; width:25px; margin: 0; padding:0")  
+    //Initially didn't work because divs were being created and not appended to the DOM after each loop so appendment had to be added in the loop. 
+    container.appendChild(div1);
 }
 
 body.setAttribute("style", "background-color:#771011;")
 
 
 
-container.appendChild(div1);
