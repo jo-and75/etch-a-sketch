e057d08d3e1f8884e5208837f3248b32bf365493
jo-and75 <jaotika@gmail.com>
2024-05-20 21:56:39 +0300

    Change display of container to flex

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 const body = document.querySelector("body");   
 const container = document.getElementById("container");  
 
-container.setAttribute("style", "border: solid purple; display:flex; width:auto; height: auto;") 
+container.setAttribute("style", "border: solid purple; display:flex;") 
 
 
 function createGrid(size) { 
     for(let i = 1; i < size ; i++){    
     const column = document.createElement("div"); 
-    column.setAttribute("style","border: solid black; height: 30px; width: 30px;"); 
+    column.setAttribute("style","border: solid black; height: 25px; width: 25px;"); 
     container.appendChild(column);   
 
         for (let j = 1; j <= size; j++){ 
             const row = document.createElement("div"); 
-            row.setAttribute("style","border: solid black; height: 30px; width: 30px;"); 
+            row.setAttribute("style","border: solid black; height: 25px; width: 25px;"); 
             column.appendChild(row);
         }
     }  
