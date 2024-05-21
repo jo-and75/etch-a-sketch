7ad5cce06c52732e7f64757db6b4985528c1afbf
jo-and75 <jaotika@gmail.com>
2024-05-21 15:57:57 +0300

    Remove border outlines of columns and rows

--- main.js
+++ main.js
@@ -12,12 +12,12 @@ function createGrid(size) {
 
     for(let i = 0; i < size ; i++){    
     const column = document.createElement("div"); 
-    column.setAttribute("style","display: flex; flex-direction: column; box-sizing: border-box; border: solid black; border:0;"); 
+    column.setAttribute("style","display: flex; flex-direction: column; box-sizing: border-box; border:0;"); 
     container.appendChild(column);   
 
         for (let j = 0; j < size; j++){ 
             const row = document.createElement("div"); 
-            row.setAttribute("style",`border: solid black; height:${cellSize}px ; width: ${cellSize}px; box-sizing: border-box;`); 
+            row.setAttribute("style",`height:${cellSize}px ; width: ${cellSize}px; box-sizing: border-box;`); 
             column.appendChild(row);
         }
     }   
@@ -25,12 +25,13 @@ function createGrid(size) {
     container.style.display = "flex"; 
     container.style.width = containerWidth + "px"; // Need to add px cause css dictates unit needs to be defined
     container.style.height = containerHeight + "px";   
-    container.style.border = "solid purple";      
+    container.style.border = "solid purple";       
+    container.style.backgroundColor = "#F3DBB2"
 }
 
 createGrid(17);
 
-body.setAttribute("style", "background-color:#771011;") 
+body.setAttribute("style", "background-color:#754B2F") 
 
 
 
