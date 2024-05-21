e9213af5b5600398a6bb3d558ae68695fc55ad17
jo-and75 <jaotika@gmail.com>
2024-05-21 15:47:44 +0300

    Change border of column constant to zero.
    - This reduces size of grid making it proportional to container size.

--- main.js
+++ main.js
@@ -1,34 +1,31 @@
 const body = document.querySelector("body");   
 const container = document.getElementById("container");  
 
-container.setAttribute("style", "border: solid purple; display:flex;") 
+function createGrid(size) {     
 
-
-
-function createGrid(size) {  
-
-    const containerWidth = size * 25;
-    const containerHeight = size * 25;
-
-    container.style.width = containerWidth + "px"; // Need to add px cause css dictates unit needs to be defined
-    container.style.height = containerHeight + "px";   
-
-
+    const cellSize = 25;  
+    const containerWidth = size * cellSize;
+    const containerHeight = size * cellSize;  
 
     for(let i = 0; i < size ; i++){    
     const column = document.createElement("div"); 
-    column.setAttribute("style","display: flex; flex-direction: column; box-sizing: border-box; border: solid black;"); 
+    column.setAttribute("style","display: flex; flex-direction: column; box-sizing: border-box; border: solid black; border:0;"); 
     container.appendChild(column);   
 
         for (let j = 0; j < size; j++){ 
             const row = document.createElement("div"); 
-            row.setAttribute("style","border: solid black; height: 25px; width: 25px; margin: 0; padding: 0; box-sizing: border-box;"); 
+            row.setAttribute("style",`border: solid black; height:${cellSize}px ; width: ${cellSize}px; box-sizing: border-box;`); 
             column.appendChild(row);
         }
-    }  
+    }   
+
+    container.style.display = "flex"; 
+    container.style.width = containerWidth + "px"; // Need to add px cause css dictates unit needs to be defined
+    container.style.height = containerHeight + "px";   
+    container.style.border = "solid purple";      
 }
 
-createGrid(16)
+createGrid(17);
 
 body.setAttribute("style", "background-color:#771011;") 
 
