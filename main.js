e8419381b549e69eb8180f8890c260e1dd81a73a
jo-and75 <jaotika@gmail.com>
2024-05-22 14:33:12 +0300

    Create button to adjust grid size

--- main.js
+++ main.js
@@ -2,14 +2,42 @@
 const body = document.querySelector("body");   
 const container = document.getElementById("container");   
 
+const gridSize = document.createElement("button");   
+gridSize.setAttribute("style", "display:flex;align-self: auto;")
+body.appendChild(gridSize); 
 
-body.setAttribute("style", "display:flex; border: solid blue; height: 100vh; justify-content: center; align-items: center; background-color:#754B2F")
+gridSize.addEventListener("click", () => { 
+    let userInput = prompt("Please enter a new grid size. Should be less than 100");  
+    let userNumber = parseInt(userInput); 
 
+    if(userNumber > 0 && userNumber < 100){ 
+        createGrid(userNumber);
+    }else if(typeof userNumber === "Number" && (userNumber < 0 || userNumber > 100))  { 
+        alert("The input should be greater than 0 and less than 100.");
+    }else{ 
+        alert("The input was not a valid number.");
+    }
+
+})
+
+const page = document.getElementById("page"); 
+page.setAttribute('style', "border: solid green;")
+
+
+
+
+
+
+body.setAttribute("style", "display:flex; flex-direction: column-reverse; gap: 60px; border: solid blue; width: 100vh; height: 100vh; justify-content: center; align-items: center; background-color:#754B2F")
+
+
+
+//const gridSize = prompt(parseInt())
 
 
 function createGrid(size) {     
 
-    const cellSize = 25;  
+    const cellSize = 10;  
     const containerWidth = size * cellSize;
     const containerHeight = size * cellSize;  
 
@@ -19,7 +47,6 @@ function createGrid(size) {
     container.appendChild(column);   
 
         for (let j = 0; j < size; j++){  
-            
             const row = document.createElement("div"); 
             row.setAttribute("style",`height:${cellSize}px ; width: ${cellSize}px; box-sizing: border-box;transition: background-color 0.3s;`); 
 
@@ -27,10 +54,8 @@ function createGrid(size) {
                  row.style.backgroundColor = "#754B2F"; // Change the color on hover
             });
             row.addEventListener("mouseleave", () => {
-              row.style.backgroundColor = "#F3DBB2"; // Reset the color when mouse leaves
+              row.style.backgroundColor = "#754B2F"//"#F3DBB2"; // Reset the color when mouse leaves
             });
-
-
             column.appendChild(row);
         }
     }   
@@ -42,7 +67,7 @@ function createGrid(size) {
     container.style.backgroundColor = "#F3DBB2"
 }
 
-createGrid(16);
+//createGrid(16);
 
 
 
